fe19830149cd4764e26ebe7f04613865eca5b115
rapon1kt <raponikt@gmail.com>
2023-07-30 19:51:11 -0300

    feat: create file storage configuration in main server.ts

--- server/src/main/server.ts
+++ server/src/main/server.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import { userFileStorage, postFileStorage } from "../config/multer";
+import multer from "multer";
 
 // CONFIGURATIONS
 
@@ -12,3 +13,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(cors());
+
+// FILE STORAGE CONFIGURATION
+
+const userUpload = multer({ storage: userFileStorage }).single("upload");
+const postUpload = multer({ storage: postFileStorage }).array("upload", 2);
