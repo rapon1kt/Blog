c57eee7463cd4246de20d8cbbfe3afb033207e83
rapon1kt <raponikt@gmail.com>
2023-07-31 03:00:13 -0300

    feat: export default routes from routes folder

---
+++ server/src/main/routes/index.ts
@@ -0,0 +1,3 @@
+export { default as authRoutes } from "./auth/authRoutes";
+export { default as postsRoutes } from "./posts/postsRoutes";
+export { default as usersRoutes } from "./users/usersRoutes";
