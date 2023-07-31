b0a85208009b6aa564af96474aa98bcbef5b5b4f
rapon1kt <raponikt@gmail.com>
2023-07-31 02:35:36 -0300

    feat: export the users controllers

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,2 +1,4 @@
 export { default as login } from "./auth/login";
 export { default as register } from "./auth/register";
+export { default as getUser } from "./users/getUser";
+export { default as getUsers } from "./users/getUsers";
