28ad9c5c066bf95336f4906f96cde09ea6cd34e7
rapon1kt <raponikt@gmail.com>
2023-07-31 02:35:10 -0300

    feat: create getUsers controller

---
+++ server/src/controllers/users/getUsers.ts
@@ -0,0 +1,14 @@
+import { Response, Request } from "express";
+import { User } from "../../models";
+
+export default async function getUsers(res: Response, req: Request) {
+	try {
+		// get users from database
+		const users = await User.find();
+		// send the users to client
+		res.status(200).json(users);
+	} catch (error: any) {
+		// if has no permission or another error send this message
+		res.status(404).json({ message: error.message });
+	}
+}
