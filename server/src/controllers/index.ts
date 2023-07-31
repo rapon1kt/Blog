8a993b569e25ba8788e1cb6f11e1e93d103e229e
rapon1kt <raponikt@gmail.com>
2023-07-31 19:17:13 -0300

    feat: export default new controllers

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -4,3 +4,5 @@ export { default as getUser } from "./users/getUser";
 export { default as getUsers } from "./users/getUsers";
 export { default as getUserPosts } from "./posts/getUserPosts";
 export { default as getFeedPost } from "./posts/getFeedPost";
+export { default as updateUser } from "./users/updateUser";
+export { default as createPost } from "./posts/createPost";
