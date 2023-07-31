7f883fb646ea8ad8f2831412a620d9a1fe365590
rapon1kt <raponikt@gmail.com>
2023-07-31 02:59:22 -0300

    feat: create Post model

---
+++ server/src/models/post/post.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { userSchema } from "../user/user";
+
+const postSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+			max: 1500,
+		},
+		comments: {
+			type: Array,
+			optional: true,
+			default: [],
+		},
+		picturePath: {
+			type: String,
+			optional: true,
+			default: "",
+		},
+		archivePath: {
+			type: String,
+			optional: true,
+			default: "",
+		},
+		owner: userSchema,
+	},
+	{ timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
