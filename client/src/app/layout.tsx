880e0441a409eb8fb5a80ee2cb35c58c0d725f93
rapon1kt <shinnatensei@gmail.com>
2023-08-02 20:05:57 -0300

    feat: create client with create-next-app

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Empreender News",
+	description: "Created by rapon1kt",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
