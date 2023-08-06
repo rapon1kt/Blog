1cf6c4e9e5bea499f46fd558350ca93b81b2e8a8
rapon1kt <shinnatensei@gmail.com>
2023-08-06 15:33:16 -0300

    feat: export new components from components folder

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,2 +1,6 @@
 export { default as Copyright } from "./copyright/copyright";
 export { default as getUser } from "./get-user/get-user";
+export { default as Navbar } from "./navbar/navbar";
+export { default as Footer } from "./footer/footer";
+export { default as MainPosts } from "./main-posts/main-posts";
+export { default as MainPostsSkeleton } from "./main-posts-skeleton/main-posts-skeleton";
