671ec86508245daa910912699161b8dbf97be6ac
rapon1kt <shinnatensei@gmail.com>
2023-08-06 15:29:25 -0300

    feat: create default footer component

---
+++ client/src/components/footer/footer.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import { Facebook, Instagram, Twitter } from "lucide-react";
+import { Box, Grid, Link, Typography, Container } from "@mui/material";
+import { Copyright } from "..";
+
+export default function Footer() {
+	return (
+		<Box
+			component="footer"
+			sx={{
+				backgroundColor: "background.paper",
+			}}
+		>
+			<Container maxWidth="lg" sx={{ p: 5 }}>
+				<Grid container spacing={5}>
+					<Grid item xs={12} sm={4} id="about">
+						<Typography variant="h6" color="text.primary" gutterBottom>
+							Sobre Nós
+						</Typography>
+						<Typography variant="body2" color="text.secondary">
+							We are XYZ company, dedicated to providing the best service to our
+							customers.
+						</Typography>
+					</Grid>
+					<Grid item xs={12} sm={4} id="contact">
+						<Typography variant="h6" color="text.primary" gutterBottom>
+							Contato
+						</Typography>
+						<Typography variant="body2" color="text.secondary">
+							Av. Pres. Tancredo de Almeida Neves, 45 - São Judas Tadeu, Itajubá
+							- MG, 37504-066
+						</Typography>
+						<Typography variant="body2" color="text.secondary">
+							Email: secretaria@colegioempreender.com.br
+						</Typography>
+						<Typography variant="body2" color="text.secondary">
+							Telefone: (35) 3629-5736
+						</Typography>
+					</Grid>
+					<Grid item xs={12} sm={4} id="social-medias">
+						<Typography variant="h6" color="text.primary" gutterBottom>
+							Redes Sociais
+						</Typography>
+						<Link
+							href="https://www.facebook.com/colegioempreender"
+							color="alternative"
+						>
+							<Facebook />
+						</Link>
+						<Link
+							href="https://www.instagram.com/colegio.empreender"
+							color="alternative"
+							sx={{ pl: 1, pr: 1 }}
+						>
+							<Instagram />
+						</Link>
+					</Grid>
+				</Grid>
+				<Copyright
+					props={{
+						mt: "5rem",
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						justifyContet: "center",
+					}}
+				/>
+			</Container>
+		</Box>
+	);
+}
