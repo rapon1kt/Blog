a938aa7b86637de5bb83be92757836bc87f71588
rapon1kt <shinnatensei@gmail.com>
2023-08-06 15:31:21 -0300

    feat: add footer in body root

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./global.modules.css";
 import ThemeContextProvider from "@/styles/theme";
 import StateProvider from "@/state/provider/provider";
+import Footer from "@/components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
 		<html lang="en">
 			<ThemeContextProvider>
 				<body className={inter.className}>
-					<StateProvider>{children}</StateProvider>
+					<StateProvider>
+						{children}
+						<Footer />
+					</StateProvider>
 				</body>
 			</ThemeContextProvider>
 		</html>
