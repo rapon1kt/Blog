35a93d3ca120dac60a3ecf873e483b8386955c51
rapon1kt <shinnatensei@gmail.com>
2023-08-07 00:24:53 -0300

    feat: export features component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Navbar } from "./navbar/navbar";
 export { default as Footer } from "./footer/footer";
 export { default as MainPosts } from "./main-posts/main-posts";
 export { default as MainPostsSkeleton } from "./main-posts-skeleton/main-posts-skeleton";
+export { default as Features } from "./features/features";
