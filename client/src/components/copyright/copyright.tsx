43694aa8ed38967877374f9d2f8b40b2cf013e35
rapon1kt <shinnatensei@gmail.com>
2023-08-09 20:03:28 -0300

    refactor: changing styles and props of copyright

--- client/src/components/copyright/copyright.tsx
+++ client/src/components/copyright/copyright.tsx
@@ -1,13 +1,39 @@
-import { Typography, Link, SxProps, Stack, Container } from "@mui/material";
+"use client";
+import {
+	Typography,
+	Link,
+	SxProps,
+	Stack,
+	Container,
+	useTheme,
+} from "@mui/material";
 import { Heart, Instagram } from "lucide-react";
 
 export default function Copyright({ props }: { props?: SxProps }) {
+	const theme = useTheme();
+
+	const alternative = theme.palette.mode === "light" ? "#407BFF" : "#F56565";
+
 	return (
-		<Container maxWidth="lg" sx={props}>
+		<Container
+			maxWidth="lg"
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				textAlign: "center",
+				mt: 5,
+				...props,
+			}}
+		>
 			<Stack>
-				<Typography variant="subtitle1" color="text.primary">
+				<Typography variant="subtitle1" color="text.secondary">
 					{"Copyright © "}
-					<Link href="https://colegioempreender.com.br">
+					<Link
+						href="https://colegioempreender.com.br"
+						sx={{ textDecoration: "none", color: "alternative" }}
+					>
 						Colégio Empreender
 					</Link>{" "}
 					{new Date().getFullYear()}
@@ -21,10 +47,10 @@ export default function Copyright({ props }: { props?: SxProps }) {
 					sx={{ dislay: "flex", alinItems: "center", justifyContent: "center" }}
 					gutterBottom
 				>
-					Made with <Heart color="#F56565" size={18} /> by{" "}
+					Made with <Heart color={alternative} size={18} /> by{" "}
 					<Link
 						href="https://instagram.com/raponi_13"
-						style={{ color: "#F56565", textDecoration: "none" }}
+						style={{ color: alternative, textDecoration: "none" }}
 					>
 						rapon1kt
 					</Link>
