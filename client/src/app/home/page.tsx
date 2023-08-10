5a74722eba113c39c98545fd412ae8c5e8eb9cfc
rapon1kt <shinnatensei@gmail.com>
2023-08-09 23:18:08 -0300

    feat: ensure Home is a private route

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+"use client";
+import { Authorization } from "@/middlewares";
+import { useSelector } from "react-redux";
+
+function HomeContainer() {
 	return (
 		<div>
 			<h1>Home</h1>
 		</div>
 	);
+}
+
+export default function Home() {
+	const { user, token } = useSelector((state: any) => state);
+	return Authorization({ Children: HomeContainer, token, user });
 }
