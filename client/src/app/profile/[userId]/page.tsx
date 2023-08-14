86de514a7dd37a175c0478b2d43355760fa321b0
rapon1kt <shinnatensei@gmail.com>
2023-08-13 23:19:37 -0300

    feat: add inital profile structure with userComponent

--- client/src/app/profile/[userId]/page.tsx
+++ client/src/app/profile/[userId]/page.tsx
@@ -1,16 +1,72 @@
 "use client";
-import { getUser } from "@/components";
+import { Footer, Navbar, getUser } from "@/components";
+import { Box } from "@mui/material";
 import { Suspense } from "react";
 import { useSelector } from "react-redux";
+import UserProfile from "@/components/user-profile/user-profile";
 
 export default function Profile({ params }: { params: { userId: string } }) {
-	const token = useSelector((state: any) => state.token);
+	const { token, user } = useSelector((state: any) => state);
 	const userId = params.userId;
-	const newUser = getUser({ userId, token });
-
 	return (
-		<Suspense fallback={<h1>Carregando...</h1>}>
-			<pre>{newUser}</pre>
-		</Suspense>
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "space-between",
+				bgcolor: "background.paper",
+				mt: 6,
+				pt: 8,
+			}}
+		>
+			<Suspense fallback={"Carregando..."}>
+				{getUser({ userId, token }).then((user) => (
+					<Navbar isCover={false} user={user} token={token} />
+				))}
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "space-around",
+						alignItems: "center",
+						flexDirection: {
+							xs: "column",
+							md: "row",
+						},
+					}}
+				>
+					<UserProfile
+						token={token}
+						userId={userId}
+						currentUser={user}
+						user={user}
+					/>
+					<Box
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							width: {
+								lg: "25vw",
+								md: "50vw",
+								sm: "100vw",
+								xs: "100vw",
+							},
+						}}
+					></Box>
+					<Box
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							width: {
+								lg: "25vw",
+								md: "50vw",
+								sm: "100vw",
+								xs: "100vw",
+							},
+						}}
+					></Box>
+				</Box>
+			</Suspense>
+			<Footer />
+		</Box>
 	);
 }
