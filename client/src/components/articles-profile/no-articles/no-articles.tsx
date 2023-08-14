cc13c323783c64ae0de1ee60d8aea9be83649086
rapon1kt <shinnatensei@gmail.com>
2023-08-14 19:15:09 -0300

    create: no articles condition to articles component

---
+++ client/src/components/articles-profile/no-articles/no-articles.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useTheme, Typography, IconButton, Stack } from "@mui/material";
+import { User } from "@/models";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function NoArticles({
+	userId,
+	user,
+}: {
+	userId: string;
+	user: User;
+}) {
+	const theme = useTheme();
+	const router = useRouter();
+
+	return (
+		<>
+			<Typography variant="subtitle1" color="text.secondary">
+				{userId === user._id ? (
+					<Stack
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+						}}
+					>
+						Parece que você ainda não possui artigos, adicione um!
+						<IconButton
+							onClick={() => router.push(`/v1/articles/new/${user._id}`)}
+						>
+							<Plus />
+						</IconButton>
+					</Stack>
+				) : (
+					"Ops! Aparentemente este usuário não possui artigos..."
+				)}
+			</Typography>
+			<img
+				style={{
+					width: "350px",
+					height: "350px",
+				}}
+				src={
+					theme.palette.mode === "dark"
+						? "/assets/not-found-dark.png"
+						: "/assets/not-found-light.png"
+				}
+				alt="Article"
+			/>
+		</>
+	);
+}
