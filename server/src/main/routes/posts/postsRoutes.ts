a3445e622cf3142147a361b9c9649b0eac9ec63b
rapon1kt <shinnatensei@gmail.com>
2023-08-14 21:19:10 -0300

    feat: add newComment route

--- server/src/main/routes/posts/postsRoutes.ts
+++ server/src/main/routes/posts/postsRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyToken } from "../../../middleware";
-import { getFeedPost, getUserPosts } from "../../../controllers";
+import { getFeedPost, getUserPosts, newComment } from "../../../controllers";
 
 const postsRoutes = express.Router();
 
 // get feed post and especific user posts
 postsRoutes.get("/", verifyToken, getFeedPost);
 postsRoutes.get("/:userId/posts", verifyToken, getUserPosts);
+postsRoutes.put("/:postId", verifyToken, newComment);
 
 export default postsRoutes;
