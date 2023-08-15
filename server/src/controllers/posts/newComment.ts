01f63065b9edb713346794fd7bedda82a35dd3b3
rapon1kt <shinnatensei@gmail.com>
2023-08-14 21:18:16 -0300

    feat: create newComment controllerfunction to posts

---
+++ server/src/controllers/posts/newComment.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { Post } from "../../models";
+
+export default async function newComment(req: Request, res: Response) {
+	try {
+		// get postId from request params
+		const { postId } = req.params;
+		// get text and owner of comment
+		const { text, owner } = req.body;
+		// find post with postId params
+		const post = await Post.findById(postId);
+		// push new comment to post
+		post?.comments.push({ text, owner });
+		// save and send comment
+		const savedComment = await post?.save();
+		res.status(201).json(savedComment);
+	} catch (err: any) {
+		// if have an server error return an message as error.message
+		res.status(500).json({
+			message: "Não foi possível publicar seu comentário.",
+			error: err,
+		});
+	}
+}
