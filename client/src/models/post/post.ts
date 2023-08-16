ece1bd8f8ac73580b8356972f37a4ef5fa10f384
rapon1kt <shinnatensei@gmail.com>
2023-08-15 21:47:07 -0300

    refactor: create comments interface and add to post model

--- client/src/models/post/post.ts
+++ client/src/models/post/post.ts
@@ -1,11 +1,11 @@
-import User from "../user/user";
+import Comments from "../comments/comments";
 
 export default interface Post {
 	_id: string;
 	title: string;
 	description: string;
 	createdAt: Date;
-	comments?: Array<string>;
+	comments?: Array<Comments>;
 	picturesPaths?: Array<string>;
 	archivePath?: string;
 	owner: String;
