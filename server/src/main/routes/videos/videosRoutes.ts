31dfe8d3466b6d3ff7e6a266381ede4c76b50618
rapon1kt <shinnatensei@gmail.com>
2023-08-19 11:21:36 -0300

    feat: create videoRoutes with get routes

--- server/src/main/routes/videos/videosRoutes.ts
+++ server/src/main/routes/videos/videosRoutes.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { verifyToken } from "../../../middleware";
+import { getVideos, getUserVideos, getVideo } from "../../../controllers";
 
 const videosRoutes = express.Router();
 
-// routes to get a single user or all the users
-videosRoutes.get("/", verifyToken, () => {
-	console.log("Happy");
-});
+// routes to get a single video or all the videos
+videosRoutes.get("/", verifyToken, getVideos);
+videosRoutes.get("/:videoId", verifyToken, getVideo);
+
+// route to get user videos
+videosRoutes.get("/:userId/videos", verifyToken, getUserVideos);
 
 export default videosRoutes;
