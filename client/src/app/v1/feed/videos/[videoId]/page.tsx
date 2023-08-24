5bcc23c0249081bc275016f56ee3b138537d5aa3
rapon1kt <shinnatensei@gmail.com>
2023-08-24 18:22:06 -0300

    feat: create initial structure of videos feed id page

--- client/src/app/v1/feed/videos/[videoId]/page.tsx
+++ client/src/app/v1/feed/videos/[videoId]/page.tsx
@@ -1,17 +1,27 @@
 "use client";
+import { Footer, Navbar } from "@/components";
 import { Authorization } from "@/middlewares";
 import { User, Video } from "@/models";
-import { useEffect, useState } from "react";
+import { Avatar, Box, Button, Stack, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 import { useSelector } from "react-redux";
+import moment from "moment";
 
 function VideoPageContainer({
 	videoId,
 	token,
+	user,
 }: {
 	videoId: string;
 	token: string;
+	user: User;
 }) {
 	const [video, setVideo] = useState<Video>();
+	const [owner, setOwner] = useState<User>();
+
+	const router = useRouter();
 
 	useEffect(() => {
 		const getVideo = async () => {
@@ -23,15 +33,155 @@ function VideoPageContainer({
 				},
 			})
 				.then((res) => res.json())
-				.then((res) => {
+				.then(async (res: Video) => {
 					setVideo(res);
+					await fetch(`http://localhost:2007/users/${res.owner}`, {
+						method: "GET",
+						headers: {
+							"Content-Type": "application/json",
+							Authorization: `Bearer ${token}`,
+						},
+					})
+						.then((res) => res.json())
+						.then((res) => {
+							setOwner(res);
+						});
 				});
 		};
-
 		getVideo();
 	}, []);
 
-	return <h1>{video?.title}</h1>;
+	const timeAgo = moment(video?.createdAt).fromNow();
+
+	return (
+		<Box
+			sx={{
+				bgcolor: "background.default",
+			}}
+		>
+			<Navbar isCover={false} user={user} token={token} />
+			<Box
+				my="4rem"
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "start",
+					justifyContent: "space-around",
+					bgcolor: "background.paper",
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						textAlign: "start",
+						borderRadius: "12px",
+						width: "100%",
+						gap: 1,
+						p: 1,
+					}}
+				>
+					<ReactPlayer
+						controls
+						url={`/assets/videos/${video?.url}`}
+						width="100%"
+						height="70vh"
+						style={{ backgroundColor: "black" }}
+					/>
+				</Box>
+				<Box sx={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+					<Stack
+						sx={{
+							p: "1rem",
+							m: "1.5rem",
+							width: "60%",
+							display: "flex",
+							flexDirection: "row",
+							gap: "1rem",
+						}}
+					>
+						<Box>
+							<Typography variant="h4" color="text.primary">
+								{video?.title}
+							</Typography>
+							<Typography variant="body2" color="alternative" gutterBottom>
+								Publicado há {timeAgo}
+							</Typography>
+							<Typography color="text.primary" variant="h5">
+								Descrição:{" "}
+							</Typography>
+							<Typography color="text.secondary" variant="subtitle1">
+								{video?.description}
+							</Typography>
+						</Box>
+					</Stack>
+					<Stack
+						sx={{
+							p: "1rem",
+							m: "1.5rem",
+							width: "60%",
+							display: "flex",
+							flexDirection: "row",
+							gap: "1rem",
+						}}
+					>
+						{owner ? (
+							<>
+								<Avatar
+									src={`/assets/users/${owner?.avatar_url}`}
+									alt={owner?.name}
+									sx={{ width: 200, height: 200 }}
+								/>
+								<Stack>
+									<Typography variant="h6" color="text.primary">
+										{owner?.name} - {owner?.role} do {owner?.grade}
+									</Typography>
+									<Typography
+										variant="subtitle1"
+										color="text.secondary"
+										gutterBottom
+									>
+										Se interessou pela forma como o vídeo foi feito por{" "}
+										{owner?.name}? Entre em seu perfil e confira novos vídeos ou
+										compartilhe esse vídeo com seus colegas!
+									</Typography>
+									<Stack sx={{ gap: "1rem" }}>
+										<Button
+											sx={{ color: "white", bgcolor: "alternative" }}
+											onClick={() => router.push(`/profile/${owner?._id}`)}
+										>
+											Ver perfil
+										</Button>
+										<Button
+											sx={{ color: "white", bgcolor: "#0a3d8f" }}
+											onClick={() => {
+												navigator.clipboard.writeText(window.location.href);
+											}}
+										>
+											Copiar link do vídeo
+										</Button>
+									</Stack>
+								</Stack>
+							</>
+						) : (
+							<h1>carregando...</h1>
+						)}
+					</Stack>
+				</Box>
+			</Box>
+			<Box
+				sx={{
+					width: "30vw",
+				}}
+			>
+				<Typography variant="h4" sx={{ height: "60vh" }}>
+					Comentários
+				</Typography>
+			</Box>
+			<Footer />
+		</Box>
+	);
 }
 
 export default function VideoPage({ params }: { params: { videoId: string } }) {
@@ -39,7 +189,9 @@ export default function VideoPage({ params }: { params: { videoId: string } }) {
 	const user = useSelector((state: { user: User }) => state.user);
 
 	return Authorization({
-		Children: <VideoPageContainer videoId={params.videoId} token={token} />,
+		Children: (
+			<VideoPageContainer videoId={params.videoId} token={token} user={user} />
+		),
 		token,
 		user,
 	});
