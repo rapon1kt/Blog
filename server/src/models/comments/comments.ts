f4338f21912fdfa42fa0225a7313bb0ecd11a157
rapon1kt <shinnatensei@gmail.com>
2023-08-26 22:49:27 -0300

    feat: create a separeted schema to owner of comment

--- server/src/models/comments/comments.ts
+++ server/src/models/comments/comments.ts
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
+const OwnerSchema = new mongoose.Schema({
+	_id: {
+		type: mongoose.Schema.ObjectId,
+		required: true,
+	},
+	name: String,
+	avatar_url: String,
+	role: String,
+});
+
 const commentsSchema = new mongoose.Schema(
 	{
 		text: {
 			type: String,
 			required: true,
 		},
-		owner: mongoose.Types.ObjectId,
+		owner: OwnerSchema,
 	},
 	{ timestamps: true }
 );
