62fd718eafa5fa500e1b7e49807499512a0f88b5
rapon1kt <shinnatensei@gmail.com>
2023-08-31 17:33:21 -0300

    feat: create aticles feed page

--- client/src/app/v1/feed/articles/page.tsx
+++ client/src/app/v1/feed/articles/page.tsx
@@ -1,3 +1,64 @@
-export default function ArticlesFeed() {
-	return <h1>Articles Page Feed</h1>;
+"use client";
+import { Footer, Navbar, ArticleFeedComponent } from "@/components";
+import { Authorization } from "@/middlewares";
+import { Post, User } from "@/models";
+import {
+	Box,
+	Button,
+	Grid,
+	Stack,
+	Typography,
+	useMediaQuery,
+} from "@mui/material";
+import { Suspense, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+function ArticleFeedContainer({ token, user }: { token: string; user: User }) {
+	const [articles, setArticles] = useState<Post[]>();
+
+	useEffect(() => {
+		const getArticle = async () => {
+			await fetch("http://localhost:2007/posts", {
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-type": "application/json",
+				},
+			})
+				.then((res) => res.json())
+				.then((articles) => setArticles(articles));
+		};
+		getArticle();
+	}, []);
+
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				bgcolor: "background.default",
+				flexDirection: "column",
+				gap: "5rem",
+			}}
+		>
+			<Navbar isCover={false} user={user} token={token} />
+			{articles &&
+				articles.map((article) => (
+					<Suspense fallback={<h1>Carregando...</h1>}>
+						<ArticleFeedComponent article={article} token={token} />
+					</Suspense>
+				))}
+			<Footer />
+		</Box>
+	);
+}
+
+export default function ArticleFeed() {
+	const token = useSelector((state: any) => state.token);
+	const user = useSelector((state: any) => state.user);
+
+	return Authorization({
+		token,
+		user,
+		Children: <ArticleFeedContainer token={token} user={user} />,
+	});
 }
