0f372c51cf6995ab66fc589433626c087b81788d
rapon1kt <shinnatensei@gmail.com>
2024-05-12 02:49:04 -0300

    refactor: move home and profile to private routes

---
+++ client/src/app/(private)/home/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { Box } from "@mui/joy";
+
+export default function Home() {
+	return <Box></Box>;
+}
---
+++ client/src/app/(private)/profile/[userId]/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { Box } from "@mui/material";
+
+export default function Profile() {
+	return <Box></Box>;
+}
