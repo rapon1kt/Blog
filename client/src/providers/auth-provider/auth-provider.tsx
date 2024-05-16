0d007350fbe8b0bc7fcfaa4da76459638faf6176
rapon1kt <shinnatensei@gmail.com>
2024-05-16 19:39:28 -0300

    feat: create auth-provider

---
+++ client/src/providers/auth-provider/auth-provider.tsx
@@ -0,0 +1,10 @@
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+type Props = {
+	children: React.ReactNode;
+};
+
+export default function AuthProvider({ children }: Props) {
+	return <SessionProvider>{children}</SessionProvider>;
+}
